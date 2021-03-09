24a85e36e10026eca5ec60c32bc5a7be8410ffd7
theo <thk12205@gmail.com>
2021-03-09 12:54:14 -0800

    add vue.js, authentication, and basic login,logout,signup pages

---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  { path: "/signup", 
+    name: "Signup", 
+    component: Signup 
+  },
+  { path: "/about", 
+    name: "About", 
+    component: About 
+  },
+  { 
+    path: "/login", 
+    name: "Login", 
+    component: Login 
+  },
+  { 
+    path: "/logout", 
+    name: "Logout", 
+    component: Logout 
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
